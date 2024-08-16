327a755c5e93602d38d64c3ceae80214cedb3ba7
ckittur <49833842+ckittur@users.noreply.github.com>
2024-08-16 13:09:45 -0700

    Add files via upload

---
+++ AP_Association.m
@@ -0,0 +1,103 @@
+
+
+%NH
+DMN=niftiread('C:\Users\leelab\Desktop\Conn_ICA\homo_output\DMN.nii'); %Whole DMN Mask
+Ant_DMN=niftiread('C:\Users\leelab\Desktop\Conn_ICA\homo_output\anterior_DMN.nii'); %Anterior DMN Mask
+Post_DMN=niftiread('C:\Users\leelab\Desktop\Conn_ICA\homo_output\posterior_DMN.nii'); % Posterior DMN Mask
+
+ indsAll=find(DMN==1);
+ nAll=length(indsAll);
+
+
+ indsAnt=find(Ant_DMN==1);
+ nAnt=length(indsAnt);
+
+ indsPost=find(Post_DMN==1);
+nPost=length(indsPost);
+
+% totN=91*109*91;
+% NHValAll=[];
+NHValAllAnt_Post=[];
+NHValAllPost_Ant=[];
+
+
+
+ % [ind1,ind2,ind3] = ind2sub(size(DMN),find(DMN == 1));
+[ind1a,ind2a,ind3a] = ind2sub(size(Ant_DMN),find(Ant_DMN == 1));
+[ind1p,ind2p,ind3p] = ind2sub(size(Post_DMN),find(Post_DMN == 1));
+
+tic
+files=dir('*pre*.nii');
+num=1;
+for subj=1:length(files)
+    subject=files(subj).name(1:10);
+    
+
+
+    for p=1:2
+        
+        NHMat=zeros(size(DMN));
+
+        if p==1
+            {subject subj 'pre' toc}
+            subjfile=dir(strcat(subject,'*pre*.nii'));
+        else
+           {subject subj 'post' toc}
+
+            subjfile=dir(strcat(subject,'*post*.nii'));
+        end
+
+ Data=double(niftiread(subjfile.name));
+DataTruncPost=zeros(nPost,300);
+DataTruncAnt=zeros(nAnt,300);
+
+for ip =1:nPost
+ DataTruncPost(ip,:)=Data(ind1p(ip),ind2p(ip),ind3p(ip),:);
+end
+
+for ia =1:nAnt
+ DataTruncAnt(ia,:)=Data(ind1a(ia),ind2a(ia),ind3a(ia),:);
+end
+
+for ina =1:nAnt
+    DataPostTemp=[DataTruncPost;DataTruncAnt(ina,:)];
+    NHMatValsAnt_Post=(sum(corrcoef(DataPostTemp'))-1)/(nAnt);
+    zCorrVal=atanh(NHMatValsAnt_Post(end));
+    NHValAllAnt_Post(ina,num)= zCorrVal;
+    NHMat(ind1a(ina),ind2a(ina),ind3a(ina))=zCorrVal;
+     {subj p toc 100*ina/(nAnt+nPost) 'Ant'}
+end
+
+
+ for inp =1:nPost
+    DataAntTemp=[DataTruncAnt;DataTruncPost(inp,:)];
+    NHMatValsPost_Ant=(sum(corrcoef(DataAntTemp'))-1)/(nPost);
+    zCorrVal=atanh(NHMatValsPost_Ant(end));
+    NHValAllPost_Ant(inp,num)=zCorrVal;
+    NHMat(ind1p(inp),ind2p(inp),ind3p(inp))=zCorrVal;
+    {subj p toc 100*(inp+nAnt)/(nAnt+nPost) 'Post'}
+ end
+
+
+        if p==1
+            outputStruct5.(subject).pre=NHMat;
+        else
+            outputStruct5.(subject).post=NHMat;
+        end
+
+
+num=num+1;
+
+    end
+end
+
+AP_Assoc_All=mean([NHValAllPost_Ant;NHValAllAnt_Post]);
+
+AP_Assoc_Pre=APDMN(1:2:35)';
+AP_Assoc_Post=APDMN(2:2:36)';
+
+  save('outputStructTest5.mat','outputStruct5');
+
+
+
+ 
---
+++ DMN_Homogeneity.m
@@ -0,0 +1,140 @@
+%Whole DMN homogeneity
+
+
+
+DMN=niftiread('C:\Users\leelab\Desktop\Conn_ICA\homo_output\DMN.nii');
+Ant_DMN=niftiread('C:\Users\leelab\Desktop\Conn_ICA\homo_output\anterior_DMN.nii');
+Post_DMN=niftiread('C:\Users\leelab\Desktop\Conn_ICA\homo_output\posterior_DMN.nii');
+AP_DMN=Ant_DMN+Post_DMN;
+
+
+ indsAll=find(DMN==1);
+ nAll=length(indsAll);
+
+ indsAnt=find(Ant_DMN==1);
+ nAnt=length(indsAnt);
+
+ indsPost=find(Post_DMN==1);
+nPost=length(indsPost);
+
+ indsAP=find(AP_DMN==1);
+nAP=length(indsAP);
+
+
+
+[ind1,ind2,ind3] = ind2sub(size(DMN),find(DMN == 1));
+[ind1a,ind2a,ind3a] = ind2sub(size(Ant_DMN),find(Ant_DMN == 1));
+[ind1p,ind2p,ind3p] = ind2sub(size(Post_DMN),find(Post_DMN == 1));
+[ind1ap,ind2ap,ind3ap] = ind2sub(size(AP_DMN),find(AP_DMN == 1));
+
+
+
+tic
+files=dir('*pre*.nii');
+num=1;
+for subj=1:length(files)
+    subject=files(subj).name(1:10);
+    
+
+
+    for p=1:2
+        
+        % NHMat=zeros(size(DMN));
+
+        if p==1
+            {subject subj 'pre' toc}
+            subjfile=dir(strcat(subject,'*pre*.nii'));
+        else
+           {subject subj 'post' toc}
+
+            subjfile=dir(strcat(subject,'*post*.nii'));
+        end
+
+ Data=double(niftiread(subjfile.name));
+for i =1:nAll
+ DataTrunc(i,:)=Data(ind1(i),ind2(i),ind3(i),:);
+end
+
+DataTruncPost=zeros(nPost,300);
+DataTruncAnt=zeros(nAnt,300);
+
+for ip =1:nPost
+ DataTruncPost(ip,:)=Data(ind1p(ip),ind2p(ip),ind3p(ip),:);
+end
+
+for ia =1:nAnt
+ DataTruncAnt(ia,:)=Data(ind1a(ia),ind2a(ia),ind3a(ia),:);
+end
+
+DataTruncAP=[DataTruncAnt;DataTruncPost];
+
+
+% NHMat=zeros(size(DMN));
+NHMatVals=(sum(corrcoef(DataTrunc'))-1)/(nAll-1);
+NHValAll(:,num)=NHMatVals;
+
+NHMatValsAnt=(sum(corrcoef(DataTruncAnt'))-1)/(nAnt-1);
+NHValAllAnt(:,num)=NHMatValsAnt;
+
+
+NHMatValsPost=(sum(corrcoef(DataTruncPost'))-1)/(nPost-1);
+NHValAllPost(:,num)=NHMatValsPost;
+
+
+NHMatValsAP=(sum(corrcoef(DataTruncAP'))-1)/(nAP-1);
+NHValAllAP(:,num)=NHMatValsAP;
+
+
+for i =1:nAnt
+    NHMat(ind1a(i),ind2a(i),ind3a(i))=atanh(NHMatValsAnt(i));
+end
+
+for i =1:nPost
+    NHMat(ind1p(i),ind2p(i),ind3p(i))=atanh(NHMatValsPost(i));
+end
+
+% % for i =1:nAP
+% %     NHMat(ind1AP(i),ind2AP(i),ind3AP(i))=atanh(NHMatValsAP(i));
+% % end
+
+
+
+        if p==1
+            outputStruct.(subject).pre=NHMat;
+        else
+            outputStruct.(subject).post=NHMat;
+        end
+
+
+num=num+1;
+
+end
+end
+
+
+WholeDMN=mean(atanh(NHValAll));
+WholeDMN_Pre=WholeDMN(1:2:35)';
+WholeDMN_Post=WholeDMN(2:2:36)';
+
+
+
+AnteriorDMN=mean(atanh(NHValAllAnt));
+AnteriorDMN_Pre=AnteriorDMN(1:2:35)';
+AnteriorDMN_Post=AnteriorDMN(2:2:36)';
+
+posteriorDMN=mean(atanh(NHValAllPost));
+posteriorDMN_Pre=posteriorDMN(1:2:35)';
+posteriorDMN_Post=posteriorDMN(2:2:36)';
+
+
+APDMN=mean(atanh(NHValAllAP));
+APDMN_Pre=APDMN(1:2:35)';
+APDMN_Post=APDMN(2:2:36)';
+
+
+Ratio_Pre=AnteriorDMN_Pre./posteriorDMN_Pre);
+Ratio_Post=AnteriorDMN_Post./posteriorDMN_Post;
+
+
+
+
